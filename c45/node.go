f50895b02500ca9bb6fe96369b4e09747ba54026
xavier268 <xavier@fed35DellXPS>
2022-07-03 21:04:53 +0200

    wip c4.5

--- c45/node.go
+++ c45/node.go
@@ -1,28 +1,27 @@
 // Package c45 contains the c4.5 algorithm.
 package c45
 
-/* WIP
-type node struct {
-	parent Node
+type Node struct {
+	parent *Node
 	crit   int     // criteria
 	cut    float64 // cut-off
-	child  []Node  // up to 2 childs
+	child  []*Node // up to 2 childs
 	class  int     // class, if leaf
 }
 
-func (n *node) IsLeaf() bool {
+func (n *Node) IsLeaf() bool {
 	return len(n.child) == 0
 }
 
-func (n *node) GetClass() int {
+func (n *Node) GetClass() int {
 	return n.class
 }
 
-func (n *node) GetCriteria() int {
+func (n *Node) GetCriteria() int {
 	return n.crit
 }
 
-func (n *node) Select(v float64) Node {
+func (n *Node) Select(v float64) *Node {
 	if v <= n.cut {
 		return n.child[0]
 	} else {
@@ -30,10 +29,9 @@ func (n *node) Select(v float64) Node {
 	}
 }
 
-func NewNode(parent Node) Node {
-	n := new(node)
-	n.child = make([]Node, 0, 2) // anticipate 2 childs max
+func NewNode(parent *Node) *Node {
+	n := new(Node)
+	n.child = make([]*Node, 0, 2) // anticipate 2 childs max
 	n.parent = parent
 	return n
 }
-*/
